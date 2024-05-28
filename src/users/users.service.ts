aafd1b3a86db7141ca5ab74d7dcd137810eec811
nayo0513 <nayo05130513@gmail.com>
2024-05-28 11:34:59 +0900

    add process of hashing password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/create-users.input';
 import dbClient from '../dbClient';
 import { UpdateUsersDto } from './dto/update-users.input';
 import { DeleteUsersDto } from './dto/delete-users.input';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UsersService {
@@ -10,9 +11,14 @@ export class UsersService {
     return dbClient.users.findMany();
   }
 
-  create(data: CreateUserDto) {
+  async create(data: CreateUserDto) {
+    const newData = {
+      email: data.email,
+      name: data.name,
+      password: await argon2.hash(data.password),
+    };
     return dbClient.users.create({
-      data: data,
+      data: newData,
     });
   }
 
@@ -24,11 +30,15 @@ export class UsersService {
       throw new Error('User not found');
     }
 
+    const encryptPassword = data.password
+      ? await argon2.hash(data.password)
+      : null;
+
     const newData = {
       id: data.id,
       email: data.email ?? prevData.email,
       name: data.name ?? prevData.name,
-      password: data.password ?? prevData.password,
+      password: encryptPassword ?? prevData.password,
     };
     return dbClient.users.update({
       where: { id: data.id },
